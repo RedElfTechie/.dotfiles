e0e4485ee6e168ebd8b679802697c7ea2d156d09
Elfred Wee <elfredwee92@gmail.com>
2021-03-26 13:04:39 +0800

    Add transparency for more coolness

--- .emacs.d/elf_lispdir/000_add_theme_dir.el
+++ .emacs.d/elf_lispdir/000_add_theme_dir.el
@@ -21,3 +21,6 @@
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
 
+;; Change transparency
+;; (set-frame-parameter (selected-frame) 'alpha '(<active> . <inactive>))
+(set-frame-parameter (selected-frame) 'alpha '(95))
